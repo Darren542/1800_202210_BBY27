74cdd7b844a51614437118d369d1a7eb9c26bf21
Darren Luck <darrenluck@icloud.com>
2022-03-02 15:07:00 -0800

    added basic login

---
+++ scripts/firebaseAPI_bby27.js
@@ -0,0 +1,20 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDwJkiKzrebKklZjpqvmM9lpLmsleDCZAI",
+    authDomain: "comp1800-bby27.firebaseapp.com",
+    projectId: "comp1800-bby27",
+    storageBucket: "comp1800-bby27.appspot.com",
+    messagingSenderId: "1078657421061",
+    appId: "1:1078657421061:web:fceeb5733ac65fd387316c"
+  };
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();

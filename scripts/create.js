6501a0e7938ec5daaf0d29a8427f152493e7da7d
Soultey <bcitalexgibbison@gmail.com>
2022-03-02 18:45:04 -0800

    create.js created, geolocation and optimized heuristics added to create event page

---
+++ scripts/create.js
@@ -0,0 +1,14 @@
+var x = document.getElementById("inputLocation");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.value = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.value = "Latitude: " + position.coords.latitude + 
+  " Longitude: " + position.coords.longitude;
+}

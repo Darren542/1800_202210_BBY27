aa605996216171cf5505615c78e3debd92319172
Darren Luck <darrenluck@icloud.com>
2022-03-02 21:44:16 -0800

    fixed up eventPage and eventPage.js

--- scripts/eventPage.js
+++ scripts/eventPage.js
@@ -1,8 +1,10 @@
 function read_events_info() {
   db.collection("events").doc("TeL6VGcmj5kW9QAl5pGx")                                                      
     .onSnapshot(doc => {                                                               
-         console.log("current document data: " + doc.data());                          
-         document.getElementById("event-description").innerHTML = doc.data().description;      
+         console.log("current document data: " + doc.data());
+         console.log(doc.data().description);                       
+         document.querySelector("#event-description").innerHTML = doc.data().description;
+         document.querySelector("#test2").innerHTML = "test text";
     })
 }
 read_events_info();

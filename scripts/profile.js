530ab4d209252a8ced19ca153710e696c452bf47
Darren Luck <darrenluck@icloud.com>
2022-04-05 22:37:46 -0700

    Profile should load correct images

--- scripts/profile.js
+++ scripts/profile.js
@@ -59,46 +59,11 @@ function displayCards(collection) {
     //performs the search with the categories selected
     //only does category atm.
     db.collection('users').doc(userId).collection(collection).limit(5).get()
-        .then(snap => {
-            var i = 1;
-            document.getElementById("cards-go-here").innerHTML = "";
-
-            snap.forEach(doc => { //iterate thru each doc
-                var eventName = doc.data().eventName;   // get value of the "name" key
-                var description = doc.data().description;   // get value of the "details" key
-                var type = doc.data().type;   // get value of the "details" key
-                var time = doc.data().startDate;   // get value of the "details" key
-                //var time = time.toDate();
-                var eventID = doc.id;
-                console.log("docId", doc.id);
-                console.log(eventID);
-                time = time.toString();
-                time = time.slice(0, 24);
-                let newcard = cardTemplate.content.cloneNode(true);
-
-                //update title and text and image
-                newcard.querySelector('.card-title').innerHTML = eventName;
-                //newcard.querySelector('.card-title').href = "./eventPage.html?eventId=" + eventID;
-                newcard.querySelector('.card-text').innerHTML = description;
-                newcard.querySelector('.card-time').innerHTML = time;
-                newcard.querySelector('.card-image').src = "./images/" + type + ".webp"; //hikes.jpg
-
-                //give unique ids to all elements for future use
-                newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
-                newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
-                newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
-                newcard.querySelector('.card-time').setAttribute("id", "ctime" + i);
-
-                let formatedLink = "location.href='./eventPage.html?eventId=" + eventID + "'";
-                newcard.querySelector('.card').setAttribute("onClick", formatedLink);
-                //attach to gallery
-                document.getElementById("cards-go-here").appendChild(newcard);
-                //newcard.addEventListener('click' => {
-                //    alert("test");
-                //});
-                i++;
-            })
-        })
+        .then(function(snap) {
+            console.log("what is in snap here", snap);
+            showCards(snap);
+            displayImages(snap);
+        });
 }
 
 
@@ -165,7 +130,10 @@ function saveUserInfo() {
     
 }
 
-
+//-----------------------------------------------------
+// This page should not accessed by users not logged in
+// This will redirect to login page if no user
+//-----------------------------------------------------
 firebase.auth().onAuthStateChanged(user => {
   if(user){
     userId = user.uid;
@@ -187,23 +155,6 @@ myDate2 = myDate2.split("-");
 var newDate2 = new Date( myDate2[2], myDate2[1] - 1, myDate2[0]);
 //newDate2 = newDate2.getTime();
 
-function writeEvents() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var EventRef = db.collection("users").doc(userId).collection("hosting");
-
-    EventRef.add({
-        description: "First hosting event",
-        endTime: newDate,
-        eventName: "test Event",
-        owner: "test",
-        postalCode: "V7E-2T9",
-        province: "BC",
-        startDate: newDate2,
-        streetAddress: "4-4051 Garry St",
-        type: "Hockey",
-        id: "we can put id here or write with same id" 
-    });
-}
 
 setTimeout(function() { changeDisplay(displayedPage); }, 500);
 
@@ -250,3 +201,77 @@ function preview() {
 //     uploadImage();
 //   });
 
+//-----------------------------------------------------------------------------
+// function for displaying images on cards.
+// Tries to get an image from the firebase storage.
+// If there is no image to display the default image for event type is used.
+//-----------------------------------------------------------------------------
+function displayImages(snap) {
+    snap.forEach(doc => {
+        let cardId = doc.id;
+        let type = doc.data().type;
+        let card = document.getElementById(cardId);
+        //console.log("card check", card);
+
+        firebase.storage().ref('images/' + cardId).getDownloadURL()
+            .then(imgUrl => {
+                card.querySelector('.card-image').src = imgUrl;
+                //console.log("card element", card);
+                //console.log("imgUrl", imgUrl);
+            })
+            .catch((error) => {
+                //console.log("No image found ", error);
+                // This is the local images url
+                card.querySelector('.card-image').src = "./images/" + type + ".webp";
+            });
+    });
+}
+
+//--------------------------------------------------------------------------------------
+// Iterates through all the documents given as a parameter.
+// Creates a new card and inserts the documents information into it.
+// Appends the newly created card to the "cards-go-here" element on the html document.
+//--------------------------------------------------------------------------------------
+function showCards(snap) {
+    var i = 1;
+    let cardTemplate = document.getElementById("eventCardTemplate2");
+    document.getElementById("cards-go-here").innerHTML = "";
+    console.log("what is in snap", snap);
+    snap.forEach(doc => {//iterate thru each doc
+        
+        var eventName = doc.data().eventName;   // get value of the "name" key
+        var description = doc.data().description;   // get value of the "details" key
+        var type = doc.data().type;   // get value of the "details" key
+        var time = doc.data().startDate;   // get value of the "details" key
+        //var time = time.toDate();
+        var eventID = doc.id;
+        console.log("docId", doc.id);
+        console.log(eventID);
+        time = time.toString();
+        time = time.slice(0, 24);
+        let newcard = cardTemplate.content.cloneNode(true);
+
+        //update title and text and image
+        newcard.querySelector('.card-title').innerHTML = eventName;
+        //newcard.querySelector('.card-title').href = "./eventPage.html?eventId=" + eventID;
+        newcard.querySelector('.card-text').innerHTML = description;
+        newcard.querySelector('.card-time').innerHTML = time;
+        //newcard.querySelector('.card-image').src = "./images/" + type + ".webp"; //hikes.jpg
+
+        //give unique ids to all elements for future use
+        newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+        newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+        newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+        newcard.querySelector('.card-time').setAttribute("id", "ctime" + i);
+
+        let formatedLink = "location.href='./eventPage.html?eventId=" + eventID + "'";
+        newcard.querySelector('.card').setAttribute("onClick", formatedLink);
+        newcard.querySelector('.card').setAttribute("id", eventID);
+        //attach to gallery
+        document.getElementById("cards-go-here").appendChild(newcard);
+        //newcard.addEventListener('click' => {
+        //    alert("test");
+        //});
+        i++;
+    });
+}
